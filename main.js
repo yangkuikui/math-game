fc31611658e53c88405f55feafc1981e909dad8c
yangkuikui <yangkuikui@gmail.com>
2021-10-05 14:20:52 +0900

    Completed app

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const form = document.querySelector(".form")
 const field = document.querySelector(".field")
 const pointsNeeded = document.querySelector(".points-needed")
 const mistakesAllowed = document.querySelector(".mistakes-allowed")
+const progressBar = document.querySelector(".progress-inner")
+const endMessage = document.querySelector(".end-message")
+const resetBtn = document.querySelector(".reset-btn")
 
 let state = {
   score: 0,
@@ -16,6 +19,7 @@ function undateProblem() {
 }
 
 undateProblem()
+field.focus()
 
 function generateNumber(max) {
   return Math.floor(Math.random() * (max + 1))
@@ -43,9 +47,12 @@ function handleSubmit(e) {
   if (parseInt(field.value, 10) === correctAnswer) {
     state.score++
     pointsNeeded.textContent = 10 - state.score
+    renderProgressBar()
   } else {
     state.mistake++
     mistakesAllowed.textContent = 10 - state.mistake
+    problem.classList.add("animation-wrong")
+    setTimeout(() => problem.classList.remove("animation-wrong"), 450)
   }
 
   undateProblem()
@@ -57,9 +64,13 @@ function handleSubmit(e) {
 function checkLogic() {
   if (state.score == 10) {
     resetGame()
+    endMessage.textContent = "Congrats! You win."
+    showOverlay()
   }
   if (state.mistake == 2) {
     resetGame()
+    endMessage.textContent = "Sorry. You lost."
+    showOverlay()
   }
 }
 
@@ -69,4 +80,20 @@ function resetGame() {
   state.mistake = 0
   pointsNeeded.textContent = 10
   mistakesAllowed.textContent = 2
+  renderProgressBar()
 }
+
+function renderProgressBar() {
+  progressBar.style.transform = `scaleX(${state.score / 10})`
+}
+
+function showOverlay() {
+  document.body.classList.add("overlay-is-open")
+  setTimeout(() => resetBtn.focus(), 333)
+}
+
+function hideOverlay() {
+  document.body.classList.remove("overlay-is-open")
+}
+
+resetBtn.addEventListener("click", hideOverlay)
